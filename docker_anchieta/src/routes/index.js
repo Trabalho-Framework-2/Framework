3face04f02df52190ce81da01e7ed57bd59d2279
Guilherme Oliveira <guiaugusto200.321@gmail.com>
2025-10-31 19:41:43 -0300

    Add files via upload

--- docker_anchieta/src/routes/index.js
+++ docker_anchieta/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const auth = require('./auth');
 const courses = require('./courses');
+const { authMiddleware } = require('../middleware/auth');
 
 const router = Router();
-router.use('/auth', auth);
-router.use('/courses', courses);
+
+router.use('/auth', auth); // rota pública
+router.use('/courses', authMiddleware, courses); // rota protegida
 
 module.exports = router;
